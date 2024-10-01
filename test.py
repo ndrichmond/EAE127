19356f8a8f4ea7223599b56adc147f7ef474029e
NickelCarbon <49594054+NickelCarbon@users.noreply.github.com>
2024-10-01 16:58:03 -0700

    updates

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-import numpy
+import numpy as np
+from tabulate import tabulate
 
+'''
 x = 5
 b = 2
 
@@ -8,6 +10,28 @@ A = x*b
 terminal_output = "hello EAE 127 :)"
 print(terminal_output)
 
+'''
+
+x=np.linspace(0,5,6)
+
+#2D Arrays
+X = np.meshgrid(x,x)
+X=X[0]
+print(X)
+print('\n')
+
+column = [(X[0])[0],(X[1])[0], (X[2])[0], (X[3])[0], (X[4])[0], (X[5])[0]]
+
+def getColumn(two_array, column_num):
+    amount_col = len(two_array)
+    temp_column = np.zeros(amount_col)
+    for i in range(0,6):
+        temp_column[i] = two_array[i][column_num]
+    return(temp_column)
 
 
+showCol = [
+    ['Column 1', getColumn(X,0)]
+]
 
+print(tabulate(showCol))
